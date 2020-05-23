13f261992752351b6a18044975420e5b87aac804
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-05-24 00:18:54 +0200

    Update tab title whenever the number of cookies changes

--- src/state/root-saga.ts
+++ src/state/root-saga.ts
@@ -1,6 +1,7 @@
 import { put, takeEvery, all, select } from "redux-saga/effects";
 import { AnyAction } from "redux";
 import { State } from ".";
+import { abbreviateNumber } from "../abbreviate-number";
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 function* buyBuilding(action: AnyAction) {
@@ -48,11 +49,24 @@ function* persistState() {
   }
 }
 
+function* updateTabTitle() {
+  const state: State = yield select();
+  document.title =
+    abbreviateNumber(Math.floor(state.numberOfCookies)) + " cookies";
+}
+
+function* watchIncrements() {
+  yield takeEvery("INCREMENT_BY", updateTabTitle);
+  yield takeEvery("INCREMENT", updateTabTitle);
+  yield takeEvery("DECREMENT_BY", updateTabTitle);
+}
+
 export default function* rootSaga() {
   yield all([
     watchIncrementAsync(),
     watchBuyBuilding(),
     eventLoop(),
     persistState(),
+    watchIncrements(),
   ]);
 }
