88501fb46b4b02546dbdef14e94e66000cbc8ccf
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-05-24 18:25:57 +0200

    Create responsive grid

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNumberOfCookies, State } from "../state";
 import cookie from "../assets/perfectCookie.png";
-import { Building, BuildingType } from "../state/buildings";
+import { BuildingType } from "../state/buildings";
 import { abbreviateNumber } from "../abbreviate-number";
 
 function Index() {
@@ -14,15 +14,15 @@ function Index() {
   );
 
   return (
-    <div className="flex bg-gray-200">
-      <div className="flex-1 text-gray-700 text-center bg-gray-100 px-4 py-2 m-2">
-        <h1>{numberOfCookies} cookies</h1>
-        <div>per second: {cookiesPerSecond}</div>
+    <div className="grid grid-flow-row sm:grid-flow-row md:grid-flow-row lg:grid-flow-col xl:grid-flow-col">
+      <div className="flex-1 text-gray-700 text-center bg-gray-100 px-4 py-2 m-1">
+        <h1 className="text-5xl">{numberOfCookies} cookies</h1>
+        <p className="text-2xl">per second: {cookiesPerSecond}</p>
         <button onClick={() => dispatch({ type: "INCREMENT" })}>
           <img src={cookie}></img>
         </button>
       </div>
-      <div className="flex-1 text-gray-700 text-center bg-gray-100 px-4 py-2 m-2">
+      <div className="flex-1 text-gray-700 text-center bg-gray-100">
         <CookieStore />
       </div>
     </div>
@@ -31,7 +31,7 @@ function Index() {
 
 const CookieStore = () => {
   return (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2">
       {Object.values(BuildingType).map((buildingType) => (
         <BuyableBuilding key={buildingType} type={buildingType} />
       ))}
@@ -50,24 +50,37 @@ const BuyableBuilding: React.FunctionComponent<BuyableBuildingProps> = ({
   const building = useSelector((state: State) => state.buildings[type]);
 
   const dispatch = useDispatch();
-  const buyBuilding = () =>
-    dispatch({
-      type: "BUY_BUILDING",
-      payload: { type: building.type, price: building.price },
-    });
   const hasEnoughCookies = numberOfCookies >= building.price;
+
+  const buyBuilding = () =>
+    hasEnoughCookies
+      ? dispatch({
+          type: "BUY_BUILDING",
+          payload: { type: building.type, price: building.price },
+        })
+      : {};
 
   return (
     <div
-      style={{
-        backgroundColor: hasEnoughCookies ? "gray" : "white",
-        margin: "10px",
-      }}
+      onClick={buyBuilding}
+      className="grid grid-rows-2 grid-flow-col align-middle hover:bg-indigo-500 p-2"
+      style={{ cursor: "pointer" }}
     >
-      <div>{type}</div>
-      <div>Price: {abbreviateNumber(building.price)}</div>
-      {hasEnoughCookies && <button onClick={buyBuilding}>Buy</button>}
-      <div>{building.numberOfBuildingsOwned}</div>
+      <div className="row-span-1 col-span-1">
+        <p className="font-sans text-lg text-purple-600 text-left">
+          {type.toLowerCase().replace("_", " ")}
+        </p>
+      </div>
+      <div className="row-span-1 col-span-1">
+        <div className="text-left">
+          Price: {abbreviateNumber(building.price)}
+        </div>
+      </div>
+      <div className="row-span-3">
+        <p className="text-xl align-middle text-right font-semibold text-purple-700 text-opacity-50">
+          {building.numberOfBuildingsOwned}
+        </p>
+      </div>
     </div>
   );
 };
