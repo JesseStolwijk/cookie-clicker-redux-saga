052f7238815cc0c5c25cda6d7d46b8aa56a81d9b
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-05-24 20:39:34 +0200

    Improve styling

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNumberOfCookies, State } from "../state";
-import cookie from "../assets/perfectCookie.png";
+import Cookie from "../assets/iconmonstr-candy-6.svg";
 import { BuildingType } from "../state/buildings";
 import { abbreviateNumber } from "../abbreviate-number";
 
@@ -15,14 +15,20 @@ function Index() {
 
   return (
     <div className="grid grid-flow-row sm:grid-flow-row md:grid-flow-row lg:grid-flow-col xl:grid-flow-col">
-      <div className="flex-1 text-gray-700 text-center bg-gray-100 px-4 py-2 m-1">
-        <h1 className="text-5xl">{numberOfCookies} cookies</h1>
-        <p className="text-2xl">per second: {cookiesPerSecond}</p>
-        <button onClick={() => dispatch({ type: "INCREMENT" })}>
-          <img src={cookie}></img>
-        </button>
+      <div className="flex flex-col text-gray-700 max-h-screen content-around text-center px-4 py-2 m-1">
+        <div className="text-gray-700 text-center bg-gray-100 px-4 py-2 m-2">
+          <h1 className="text-5xl">{numberOfCookies} cookies</h1>
+          <p className="text-2xl">per second: {cookiesPerSecond}</p>
+        </div>
+        <div className="px-4 py-2 m-8">
+          <img
+            className="transform cursor-pointer hover:scale-110 w-64 fill-curren m-auto"
+            onClick={() => dispatch({ type: "INCREMENT" })}
+            src={Cookie}
+          />
+        </div>
       </div>
-      <div className="flex-1 text-gray-700 text-center bg-gray-100">
+      <div className="flex-1 text-gray-700 text-center">
         <CookieStore />
       </div>
     </div>
@@ -31,7 +37,7 @@ function Index() {
 
 const CookieStore = () => {
   return (
-    <div className="grid grid-cols-1 max-h-screen overflow-y-scroll">
+    <div className="grid grid-cols-1 max-h-screen lg:overflow-y-scroll xl:overflow-y-scroll">
       {Object.values(BuildingType).map((buildingType) => (
         <BuyableBuilding key={buildingType} type={buildingType} />
       ))}
@@ -60,18 +66,17 @@ const BuyableBuilding: React.FunctionComponent<BuyableBuildingProps> = ({
         })
       : {};
 
-  const backGroundClasses = hasEnoughCookies
-    ? "hover:bg-gray-200 p-2"
-    : "bg-gray-400";
+  const extraClasses = hasEnoughCookies
+    ? "hover:bg-gray-200 cursor-pointer"
+    : "bg-gray-100 opacity-25";
 
   return (
     <div
       onClick={buyBuilding}
       className={
-        "grid grid-rows-2 grid-flow-col align-middle p-2 select-none " +
-        backGroundClasses
+        "grid grid-rows-2 grid-flow-col align-middle p-2 select-none items-center " +
+        extraClasses
       }
-      style={hasEnoughCookies ? { cursor: "pointer" } : undefined}
     >
       <div className="row-span-1 col-span-1">
         <p className="font-sans text-lg text-purple-600 text-left capitalize">
